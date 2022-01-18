a10a748ee7217717486180517aba52bf27c7b59f
Oliver Nutter <mrnoname1000@riseup.net>
2022-01-18 06:00:13 -0600

    Use mp.utils.split_path

--- mpvSockets.lua
+++ mpvSockets.lua
@@ -3,16 +3,12 @@
 local utils = require 'mp.utils'
 
 local function get_temp_path()
-    local directory_seperator = package.config:match("([^\n]*)\n?")
     local example_temp_file_path = os.tmpname()
 
     -- remove generated temp file
     pcall(os.remove, example_temp_file_path)
 
-    local seperator_idx = example_temp_file_path:reverse():find(directory_seperator)
-    local temp_path_length = #example_temp_file_path - seperator_idx
-
-    return example_temp_file_path:sub(1, temp_path_length)
+    return utils.split_path(example_temp_file_path)
 end
 
 tempDir = get_temp_path()
